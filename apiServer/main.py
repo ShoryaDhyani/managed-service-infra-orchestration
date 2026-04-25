490ec3cfb1d8eaf32397e6ebd7f8f06c8405fd92
Shorya <shoryadhyani@gmail.com>
2026-04-26 01:15:34 +0530

    dummy changes

--- apiServer/main.py
+++ apiServer/main.py
@@ -13,8 +13,6 @@ from contextlib import asynccontextmanager
 from config import config
 from config import ProjectRequest
 
-
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     task = asyncio.create_task(init_redis_subscribe())
