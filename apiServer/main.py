031aa59f8da218e08b81b53120b0d1d85c0f01d5
Shorya <shoryadhyani@gmail.com>
2026-05-03 19:35:08 +0530

    change deployment

--- apiServer/main.py
+++ apiServer/main.py
@@ -145,7 +145,7 @@ async def create_project(body: ProjectRequest, request: Request):
         'status': 'queued',
         'data': {
             'projectSlug': project_slug,
-            'url': f'{proto}://{project_slug}.localhost:8000'
+            'url': f'{proto}://{project_slug}.{host}'
         }
     }
 
